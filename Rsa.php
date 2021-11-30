8e28a5106781b5b707928455793f24ac0ed8758e
Dwi Ayuni Rohana <91515332+DwiAyuni@users.noreply.github.com>
2021-11-30 18:43:29 +0700

    Update Rsa.php

--- Rsa.php
+++ Rsa.php
@@ -1,191 +1,242 @@
-<?php
-
-class Rsa {
-
-    private $p; // bilangan prima pertama
-    private $q; // bilangan prima kedua
-    private $n; // bilangan modulus
-    private $m; // bilangan totient
-    private $e; // exponen enkripsi (kunci publik untuk enkripsi)
-    private $d; // exponen dekripsi (kunci privat untuk dekripsi)
-    private $e_arr; // array dari e yang tersedia
-    private $d_arr; // array dari sekian d yang tersedia
-
-
-    function gen_key($p, $q) {
-        $this->p = $p;
-        $this->q = $q;
-        $this->n = $p * $q;
-        $this->m = ($p - 1) * ($q - 1);
-        $this->gen_e($this->m);
-        $this->gen_d($this->m, $this->e);
-    }
+<?php 
     
-    // function to check wether a number is a prime number
-    function check_prime($num){ 
-    	if ($num == 1) {
-    	    return false; 
-    	}
-    	
-    	for ($i = 2; $i <= $num / 2; $i++){ 
-    		if ($num % $i == 0) {
-    			return false; 
-    		}
-    	}
-    	
-    	return true; 
-    } 
+    include 'Rsa.php';
 
-    // function to encrypt a text with the public key pair (e, n)
-    // parameter pertama: teks yang akan dienkripsi
-    // parameter kedua: kunci publik untuk enkripsi (e)
-    // parameter ketiga: bilangan modulus
-    function encrypt($text, $e, $n) {
-        // teks di pisah per 1 karakter menjadi bentuk array
-        $text_arr = str_split($text);
-
-        foreach ($text_arr as $text_arr) {
-            $ord_text = ord($text_arr); // ubah karakter ke angka desimal
-            // echo "\$ord_text = $ord_text<br>";
-            // echo "\$e = $e<br>";
-            // echo "\$n = $n<br>";
-            $chip_arr[] = bcpowmod($ord_text, $e, $n); // rumus enkripsi
-        }
-
-        $ord_chip = implode(",", $chip_arr); // array desimal disatukan dengan pemisah koma
-        return $ord_chip;
-    }
-
-    // function to decrypt a text with the private key pair (d, n)
-    // parameter pertama: teks yang akan didekripsi
-    // parameter kedua: kunci privat untuk dekripsi (d)
-    // parameter ketiga: bilangan pembagi/modulus
-    function decrypt($ord_chip, $d, $n) {
-        $chip_arr = preg_split("/[^0-9]/", $ord_chip); // array desimal dipisah sesuai koma
-        
-        foreach ($chip_arr as $chip_arr) {
-            // echo "\$chip_arr = $chip_arr<br>";
-            // echo "\$d = $d<br>";
-            // echo "\$n = $n<br>";
-            $ord_chip = bcpowmod($chip_arr, $d, $n); // rumus dekripsi
-            // echo "\$ord_chip: " . $ord_chip . "<br>";
-            $text_arr[] = chr($ord_chip); // ubah angka desimal ke karakter/huruf
-        }
-        
-        $text = implode("", $text_arr); // satukan karakter tanpa pemisah
-        return $text;
-    }
-
-
-    function gen_e($m) {
-        // e[]=null;
-        for ($i = 8; $i < $m; $i++) {
-            if ( $this->gcd($m, $i) == 1 ) {
-                $e[] = $i;
-                // echo "<br>i di e=".$i;
-            }
-        }
-        
-        // $this->e = end(array_values($e));
-        $this->e_arr = $e;
-        $this->e = $e[0];
-    }
-
-    function gen_d($m, $e, $limit = 100) {
-        $j=0;
-        for ($i = 7; $i < $limit; $i++) {
-
-            $result = (1 + ( $i * $m )) % $e;
-            // echo "<br>m=".$m;
-            // echo "<br>result di d=".$result;
-            if ( $result == 0 ) {
-                $the_d[] = (1 + ( $i * $m )) / $e;
-                // echo "<br>result=".$result;
-                // echo "<br>nilai i=".$i;
-                // echo "<br>d=".$the_d[$j];
-                $j++;
-            }
-        }
-
-        $this->d_arr = $the_d; 
-        $this->d = $the_d[0];
-    }
-
-    // recursive function to calculate the Greatest common divisor from two number
-    function gcd($x, $y) {
-        return (bcmod($x, $y)) ? $this->gcd($y, bcmod($x, $y)) : $y;
-    }
-
-    // getter
-    function get_n() {
-        return $this->n;
-    }
-
-    function get_m() {
-        return $this->m;
-    }
-
-    function get_p() {
-        return $this->p;
-    }
-
-    function get_q() {
-        return $this->q;
-    }
-    
-    function get_e() {
-        return $this->e;
-    }
-    
-    function get_d() {
-        return $this->d;
-    }
-
-    function get_e_arr() {
-        return $this->e_arr;
-    }
-
-    function get_d_arr() {
-        return $this->d_arr;
-    }
-
-    // setter
-    function set_p($p) {
-        $this->p = $p;
-        $this->q = $q;
-        $this->n = $p * $q;
-        $this->m = ($p - 1) * ($q - 1);
-        $this->gen_e($this->m);
-        $this->gen_d($this->m, $this->e);
-    }
-
-    function set_q($q) {
-        $this->q = $q;
-        $this->n = $p * $q;
-        $this->m = ($p - 1) * ($q - 1);
-        $this->gen_e($this->m);
-        $this->gen_d($this->m, $this->e);
-    }
-    
-    function set_n($n) {
-        $this->n = $n;
-    }
-
-    function set_m($m) {
-        $this->m = $m;
-        $this->gen_e($this->m);
-        $this->gen_d($this->m, $this->e);
-    }
-    
-    function set_e($e) {
-        $this->e = $e;
-        $this->gen_d($this->m, $this->e);
-    }
-    
-    function set_d($d) {
-        $this->d = $d;
-    }
-    
-}
+    $rsa = new Rsa();
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>naomi gaming</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+        crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+    <header class="container bg-primary text-white">
+        <div class="row">
+            <div class="col-12 py-4 text-center">
+                <h1 class="display-1"></h1>
+                <p class="lead">Program Alogoritma RSA Pada PHP</p>
+            </div>
+        </div>
+    </header>
+
+    <main class="container border">
+        <div class="row">
+            <div class="col-md-4 py-5">
+            <div class="card bg-primary">
+                  <h3 class="card-header text-white lead">Masukkan Nilai p, Nilai q, dan Teks</h3>
+              <div class="card-body bg-light">
+                <div class="card-text">
+                
+                  <form method="POST">
+                    <div class="form-floating mb-3">
+                      <input type="number" class="form-control" id="pValue" aria-describedby="pValueHelp" name="p_value" value="<?= isset($_POST['p_value']) ? $_POST['p_value'] : ''; ?>">
+                      <label for="pValue">Nilai p</label>
+                      <div id="pValueHelp" class="form-text">p harus bilangan prima lebih besar dari 7</div>
+                    </div>
+                    <div class="form-floating mb-3">
+                      <input type="number" class="form-control" id="qValue" aria-describedby="qValueHelp" name="q_value" value="<?= isset($_POST['q_value']) ? $_POST['q_value'] : ''; ?>">
+                      <label for="qValue">Nilai q</label>
+                      <div id="qValueHelp" class="form-text">q harus bilangan prima lebih besar dari 7 dan berbeda dengan p</div>
+                    </div>
+                    <div class="form-floating mb-3">
+                      <textarea class="form-control" id="textValue" rows="3" aria-describedby="textHelp" name="text_value" placeholder="Teks" style="height: 100px"><?= isset($_POST['text_value']) ? $_POST['text_value'] : ''; ?></textarea>
+                      <label for="textValue" class="">Teks</label>
+                      <div id="textHelp" class="form-text">Ketik teks yang ingin di enkripsi. Atau chipertext yang ingin di dekripsi. Contoh chipertext: 72,89,45,60</div>
+                    </div>
+                    <input type="submit" name="encrypt" class="btn btn-danger" value="Enkripsi">
+                    <input type="submit" name="decrypt" class="btn btn-primary" value="Dekripsi">
+                  </form>
+                </div>
+              </div>
+          </div>
+        </div>
+
+        <?php
+          if ( ( isset($_POST['encrypt']) || isset($_POST['decrypt']) ) && isset($_POST['p_value']) && isset($_POST['q_value']) && isset($_POST['text_value']) ) {
+            $p = $_POST['p_value'];
+            $q = $_POST['q_value'];
+            $text = $_POST['text_value'];
+            
+            if ( isset($_POST['decrypt']) && preg_match("/[a-zA-Z]/", $text) ) {
+                echo '<script>alert("Isi teks untuk proses dekripsi harus berupa angka desimal yang dipisah dengan spasi/simbol.\nContoh: \'72,89,45,60\' atau \'72 89 45 60\'");</script>';
+                exit();
+                header('Location:index.php');
+            }
+            
+            if ( $p == $q || $p < 10 || $q < 10 || $rsa->check_prime($p) == false || $rsa->check_prime($q) == false ) {
+                echo '<script>alert("Isi p dan q harus berupa bilangan prima dan harus lebih besar dari 7\np dan q tidak boleh sama");</script>';
+                exit();
+                header('Location:index.php');
+            }
+            
+            
+            $rsa->gen_key($p, $q);
+
+        ?>
+
+            <div class="col-md-8 py-5">
+            <div class="card bg-primary">
+              <div class="card-header bg-primary">
+                <h1 class="lead text-white">Hasil </h1>
+              </div>
+              <div class="card-body bg-light">
+                  <div class="mb-3 row">
+                    <label for="nilai-p" class="col-sm-1 col-form-label">Nilai p</label>
+                    <div class="col-sm-3">
+                      <input disabled type="text" class="form-control" id="nilai-p" value="<?= $rsa->get_p() ?>">
+                    </div>
+                    <label for="nilai-q" class="col-sm-1 col-form-label">Nilai q</label>
+                    <div class="col-sm-3">
+                      <input disabled type="text" class="form-control" id="nilai-q" value="<?= $rsa->get_q() ?>">
+                    </div>
+                    <label for="nilai-n" class="col-sm-1 col-form-label">Nilai n</label>
+                    <div class="col-sm-3">
+                      <input disabled type="text" class="form-control" id="nilai-n" value="<?= $rsa->get_n() ?>">
+                    </div>
+                  </div>
+                  <div class="mb-3 row">
+                    <label for="nilai-m" class="col-sm-1 col-form-label">Nilai m</label>
+                    <div class="col-sm-3">
+                      <input disabled type="text" class="form-control" id="nilai-m" value="<?= $rsa->get_m() ?>">
+                    </div>
+                    <label for="nilai-e" class="col-sm-1 col-form-label">Nilai e</label>
+                    <div class="col-sm-3">
+                      <input disabled type="text" class="form-control" id="nilai-e" value="<?= $rsa->get_e() ?>">
+                    </div>
+                    <label for="nilai-d" class="col-sm-1 col-form-label">Nilai d</label>
+                    <div class="col-sm-3">
+                      <input disabled type="text" class="form-control" id="nilai-d" value="<?= $rsa->get_d() ?>">
+                    </div>
+                  </div>
+                  
+                  <?php
+                    if ( isset($_POST['encrypt']) ) {
+                      $encrypted = $rsa->encrypt($text, $rsa->get_e(), $rsa->get_n());
+                      $encrypted_arr = preg_split('/[^a-zA-Z0-9]/', $encrypted);
+                    ?>
+                    <div class="table-responsive">
+                      <table class="table table-bordered">
+                          <tr>
+                            <td>Plainteks</td>
+                            <?php
+                              foreach (str_split($text) as $tx) {
+                                echo "<td>$tx</td>";
+                              }
+                            ?>
+                          </tr>
+                          <tr>
+                            <td>Plainteks (DEC)</td>
+                            <?php
+                              foreach (str_split($text) as $tx) {
+                                echo "<td>" . ord($tx) . "</td>";
+                              }
+                            ?>
+                          </tr>
+                          <tr>
+                            <td>Chiperteks (DEC)</td>
+                            <?php
+                              foreach ($encrypted_arr as $en) {
+                                echo "<td>$en</td>";
+                              }
+                            ?>
+                          </tr>                        
+                          <tr>
+                            <td>Chiperteks</td>
+                            <?php
+                              foreach ($encrypted_arr as $en) {
+                                echo "<td>" . chr($en) . "</td>";
+                              }
+                            ?>
+                          </tr>
+                      </table>
+                    </div>
+                   <div class="mb-3"> 
+                     <label for="chipertextResultValue" class="form-labe;">Chiperteks:</label> 
+                    <textarea disabled class="form-control" id="chipertextResultValue" aria-describedby="chipertextResultHelp" name="chipertextResult_value" placeholder="Teks" style="height: 120px"><?= $encrypted ?></textarea>
+                   </div>
+                   <?php
+                    }
+                  ?>
+                  <?php
+                    if ( isset($_POST['decrypt']) ) {
+                      $decrypted = $rsa->decrypt($text, $rsa->get_d(), $rsa->get_n());
+                      $text_arr = preg_split('/[^a-zA-Z0-9]/', $text);
+                  ?>
+                  <div class="table-responsive">
+                      <table class="table table-bordered">
+                        <tr>
+                          <td>Chiperteks</td>
+                          <?php
+                            foreach ($text_arr as $tx) {
+                              echo "<td>" . chr($tx) . "</td>";
+                            }
+                            ?>
+                        </tr>
+                        <tr>
+                          <td>Chiperteks (DEC)</td>
+                          <?php
+                            foreach ($text_arr as $tx) {
+                              echo "<td>$tx</td>";
+                            }
+                          ?>
+                        </tr>
+                        <tr>
+                          <td>Plainteks (DEC)</td>
+                          <?php
+                            foreach (str_split($decrypted) as $de) {
+                              echo "<td>" . ord($de) . "</td>";
+                            }
+                          ?>
+                        </tr>                        
+                        <tr>
+                          <td>Plainteks</td>
+                          <?php
+                            foreach (str_split($decrypted) as $de) {
+                              echo "<td>$de</td>";
+                            }
+                          ?>
+                        </tr>
+                      </table>
+                    </div>
+                   <div class="mb-3"> 
+                     <label for="plaintextResultValue" class="form-label">Plainteks:</label> 
+                    <textarea disabled class="form-control" id="plaintextResultValue" aria-describedby="plaintextResultHelp" name="plaintextResult_value" placeholder="Teks" style="height: 120px"><?= $decrypted ?></textarea>
+                   </div>
+                  <?php
+                    }
+                  ?>
+                  
+              </div>
+          </div>
+        </div>
+          <?php } ?>
+        </div>
+            </div>
+            
+        </div>
+    </main>
+
+    <footer class="container bg-primary text-white">
+        <div class="row">
+            <div class="col-12 py-4">
+                
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
